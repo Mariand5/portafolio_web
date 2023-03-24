21e83373f9988655158264f3e3b0e509010dd726
Mariand5 <mariana.da.vila@mi.unc.edu.ar>
2023-03-24 20:44:52 -0300

    Pulido de frontend
    Personalización de colores, fuentes, carga de información personal.

--- portafolio_web/backEnd/src/main/java/com/portfolio/mdv/Controller/PersonaController.java
+++ portafolio_web/backEnd/src/main/java/com/portfolio/mdv/Controller/PersonaController.java
@@ -8,6 +8,7 @@ import com.portfolio.mdv.Entity.Persona;
 import com.portfolio.mdv.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:53444")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
--- portafolio_web/frontEnd/src/app/componentes/acerca-de/acerca-de.component.ts
+++ portafolio_web/frontEnd/src/app/componentes/acerca-de/acerca-de.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { persona } from 'src/app/model/persona.model';
+import { PersonaService } from 'src/app/service/persona.service';
 
 @Component({
   selector: 'app-acerca-de',
   templateUrl: './acerca-de.component.html',
   styleUrls: ['./acerca-de.component.css']
 })
-export class AcercaDeComponent {
-
+export class AcercaDeComponent implements OnInit {
+  persona: persona = new persona("","","");
+  //PersonaService hace referencia al typescript modificado en service
+  constructor(public personaService: PersonaService) {}
+  //subscribe conecta el observable con evetos, metodo que obtiene la información que fue modificada por el observable
+  //cuando cambie la condición se va a guardar en data, el subscribe escucha el data y lo pasa al service, el service lo pasa al backend
+  ngOnInit(): void {
+    this.personaService.getPersona().subscribe(data => {this.persona = data})
+  }
 }
